ed0acdbdcb231c95e85f0c3fa52c8d8c250b2c2e
Daniel Platt <github@ofdan.co.uk>
2017-03-30 08:37:30 +0100

    Get symfony talking to the database
    my can’t I have parameters.yml and parameters.php?

--- code/app/config/env_parameters.php
+++ code/app/config/env_parameters.php
@@ -1,6 +1,7 @@
 <?php
 
 $container->setParameter('database_host', 'database');
+$container->setParameter('database_port', 3306);
 $container->setParameter('database_name', $_ENV['MYSQL_DATABASE']);
 $container->setParameter('database_user', $_ENV['MYSQL_USER']);
-$container->setParameter('database_pass', $_ENV['MYSQL_PASS']);
+$container->setParameter('database_password', $_ENV['MYSQL_PASS']);
