c191b468c801a8ae65c6db652887f81a3b7a2980
Daniel Platt <github@ofdan.co.uk>
2017-04-02 12:07:14 +0100

    allow cache to be rebuilt in dev mode

--- code/app/config/env_parameters.php
+++ code/app/config/env_parameters.php
@@ -1,7 +1,9 @@
 <?php
 
-$container->setParameter('database_host', 'database');
-$container->setParameter('database_port', 3306);
-$container->setParameter('database_name', $_ENV['MYSQL_DATABASE']);
-$container->setParameter('database_user', $_ENV['MYSQL_USER']);
-$container->setParameter('database_password', $_ENV['MYSQL_PASS']);
+if (isset($_ENV['MYSQL_DATABASE'])) {
+    $container->setParameter('database_host', 'database');
+    $container->setParameter('database_port', 3306);
+    $container->setParameter('database_name', $_ENV['MYSQL_DATABASE']);
+    $container->setParameter('database_user', $_ENV['MYSQL_USER']);
+    $container->setParameter('database_password', $_ENV['MYSQL_PASS']);
+}
