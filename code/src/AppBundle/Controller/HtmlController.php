0e9f8c6933502ffd9ce82819dd4d4d17dd67f007
Daniel Platt <github@ofdan.co.uk>
2017-04-14 18:13:37 +0100

    display current path

--- code/src/AppBundle/Controller/HtmlController.php
+++ code/src/AppBundle/Controller/HtmlController.php
@@ -12,12 +12,13 @@ class HtmlController extends Controller
      * @Route("/", name="homepage")
      * @Route("/html/{path}", name="html", requirements={"path"=".*"})
      */
-    public function htmlAction(Request $request)
+    public function htmlAction(Request $request, $path = 'homepage')
     {
         $this->get('register_impression')->impression($request);
         $paths = $this->get('routes')->getPaths();
 
         return $this->render('html/page.html.twig', [
+            'page' => $path,
             'paths' => $paths,
         ]);
     }
