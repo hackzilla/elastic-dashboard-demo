32dfcc5576e02a9dd9279a69c5bce0954eafcb40
Daniel Platt <github@ofdan.co.uk>
2017-04-14 11:23:44 +0100

    run elastic setup from application

--- opt/startup.sh
+++ opt/startup.sh
@@ -4,5 +4,8 @@ sudo -u www-data /var/www/html/bin/console cache:clear --env=prod
 sudo -u www-data /var/www/html/bin/console cache:clear --env=dev
 
 sudo -u www-data /var/www/html/bin/console doctrine:migrations:migrate --no-interaction
+sudo -u www-data /var/www/html/bin/console app:elastic:create-index
+sudo -u www-data /var/www/html/bin/console app:elastic:create-pipeline
+sudo -u www-data /var/www/html/bin/console app:elastic:create-mapping
 
 /usr/local/bin/apache2-foreground
